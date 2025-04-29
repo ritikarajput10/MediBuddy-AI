03017755ff54ab38a2cd6b628afe5ce1256607d0
Ritika Raj <ritika.sctcode@gmail.com>
2025-04-29 22:11:46 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ritika Rajput',
+    author_email= 'ritika.sctcode@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
